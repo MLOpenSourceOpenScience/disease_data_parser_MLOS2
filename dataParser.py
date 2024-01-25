f1647ae975ff09743736c66712098f9ab9a8faf2
Mahi <mahipasarkar@gmail.com>
2024-01-24 20:43:40 -0500

    Starting commit

---
+++ dataParser.py
@@ -0,0 +1,35 @@
+import sys
+import importlib;
+from datetime import datetime;
+from Modules.PDFoperators import *
+
+
+if __name__ == "__main__":
+    
+    n = len(sys.argv)
+    if n == 2 and sys.argv[1] == "-h": #TODO: improve help options
+        print("Usage: dataParser.py <folder-to-parse/> <output-file.csv> <parsing-model.py>") 
+        print("Arg 1: folder of PDFs to parse. They should all be compatible with the same parsing model")
+        print("Arg 2: output file, in csv format")
+        print("Arg 3: parsing model. PDF will be converted to text, but model will convert text to array data")
+        quit()
+    if (n<4):
+        print("Invalid number of arguments! Correct usage: dataParser.py <folder-to-parse/> <output-file.csv> <parsing-model.py>")
+        print("run dataParser.py -h for more information")
+        quit()
+    
+    #Import Arguments
+    inFolder = sys.argv[1]  # Arg 1: folder of PDFs to parse. They should all be compatible with the same parsing model
+    outFile = sys.argv[2]   # Arg 2: output file, in csv format
+    modelFile = sys.argv[3] # Arg 3: parsing model. PDF will be converted to text, but model will convert text to array data
+
+    model = importlib.import_module(modelFile) 
+
+    formattedPDF = formatPDF(filename) #Alan
+    rtfData = PDFtoRTF(formattedPDF) #Alan
+    importantText,timestamps = model.extractDataFromRTF(rtfData) #Eoin
+    table = model.convertToTable(importantText,timestamps) #Mahi
+
+    print(table)
+
+
