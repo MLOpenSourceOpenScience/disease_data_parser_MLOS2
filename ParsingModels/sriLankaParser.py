47b67d672c20683b8a576a7629dd43dd4e3d7c4a
Mahi <mahipasarkar@gmail.com>
2024-01-24 21:08:46 -0500

    improved imports, still need to import llama interface

---
+++ ParsingModels/sriLankaParser.py
@@ -0,0 +1,5 @@
+from SriLankaModules.convertToTable import *
+from SriLankaModules.rtfExtractor import *
+
+if __name__ == '__main__':
+    print("testing")
--- dataParser.py
+++ dataParser.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
         print("Usage: dataParser.py <folder-to-parse/> <output-file.csv> <parsing-model.py>") 
         print("Arg 1: folder of PDFs to parse. They should all be compatible with the same parsing model")
         print("Arg 2: output file, in csv format")
-        print("Arg 3: parsing model. PDF will be converted to text, but model will convert text to array data")
+        print("Arg 3: parsing model. PDF will be converted to text, but model will convert text to array data. If it is in a folder, replace / with . in the path so python can import properly")
         quit()
     if (n<4):
         print("Invalid number of arguments! Correct usage: dataParser.py <folder-to-parse/> <output-file.csv> <parsing-model.py>")
+        print("Example: dataParser.py Data Output/data.csv ParsingModels.sriLankaParser.py")
         print("run dataParser.py -h for more information")
         quit()
     
@@ -25,8 +26,9 @@ if __name__ == "__main__":
 
     model = importlib.import_module(modelFile) 
 
-    formattedPDF = formatPDF(filename) #Alan
-    rtfData = PDFtoRTF(formattedPDF) #Alan
+    #formattedPDF = formatPDF(filename) #Alan
+    #rtfData = PDFtoRTF(formattedPDF) #Alan
+    rtfData = "ahhhh"
     importantText,timestamps = model.extractDataFromRTF(rtfData) #Eoin
     table = model.convertToTable(importantText,timestamps) #Mahi
 
