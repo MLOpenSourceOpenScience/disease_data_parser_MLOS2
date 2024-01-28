65ba45228a52155a1f89e8e90c2b732d9ec74528
Mahi <mahipasarkar@gmail.com>
2024-01-28 11:05:56 -0500

    Moved LLaMaInterface

--- ParsingModels/SriLankaModules/convertToTable.py
+++ ParsingModels/SriLankaModules/convertToTable.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("...")
 sys.path.append("..")
 
-from Modules.LLaMaInterface import separateCellHeaders
+from LLaMa.LLaMaInterface import separateCellHeaders
 from datetime import datetime,timedelta;
 
 
