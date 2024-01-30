6a9aac9160b80e06481c99a6cc6e0806669f5714
EggyRepublic <Eggyrepublic@gmail.com>
2024-01-30 13:27:56 -0500

    added support for no output argument

--- Modules/pdfExtractor.py
+++ Modules/pdfExtractor.py
@@ -2,10 +2,14 @@
 
 import subprocess as sp
 import pathlib
+import os
 
-def PDFtoRTF(path, output_file):
+def PDFtoRTF(path, output_file=None):
     #Generate a text rendering of a PDF file in the form of a list of lines.
-    args = ['pdftotext', '-layout', path, output_file]
+    if output_file is None:
+        args = ['pdftotext', '-layout', path]
+    else:
+        args = ['pdftotext', '-layout', path, output_file]
     cp = sp.run(
       args, stdout=sp.PIPE, stderr=sp.DEVNULL,
       check=True, text=True
@@ -13,10 +17,14 @@ def PDFtoRTF(path, output_file):
     return cp.stdout
 
 def runner():
-    input_file = 'Data/Dengue/dengue sri lanka.pdf'
-    output_name = input_file.split('/')[-1]
-    pathlib.Path('PDFAsText').mkdir(parents=True, exist_ok=True) 
-    print(PDFtoRTF(input_file, f'PDFAsText/{output_name}.txt'))
+    this_path = os.getcwd()
+    this_path = this_path[:this_path.rfind('\\')]
+    input_file = this_path + '\\Data\\Dengue\\sri lanka.pdf'
+    print(input_file)
+    output_name = input_file[input_file.rfind('\\') + 1:]
+    pathlib.Path('PDFAsText').mkdir(parents=True, exist_ok=True)
+    print(PDFtoRTF(input_file))
+    #print(PDFtoRTF(input_file, f'PDFAsText/{output_name}.txt'))
 
 if __name__ == '__main__':
     runner()
