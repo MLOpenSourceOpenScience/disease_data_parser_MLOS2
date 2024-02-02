8a53adf8719835eedd07096348a431e1891090d3
Mahi <mahipasarkar@gmail.com>
2024-02-01 23:23:18 -0500

    progress bump

---
+++ Modules/__init__.py
@@ -0,0 +1,5 @@
+
+from Modules.DieseaseHeader import *
+from Modules.LocationInterface import *
+from Modules.pdfExtractor import *
+from Modules.PDFoperators import *
---
+++ ParsingModels/__init__.py
@@ -0,0 +1,6 @@
+
+
+from ParsingModels.LLaMa.LLaMaInterface import *
+from ParsingModels.SriLankaModules.convertToTable import *
+from ParsingModels.SriLankaModules.rtfExtractor import *
+from ParsingModels.sriLankaParser import *
