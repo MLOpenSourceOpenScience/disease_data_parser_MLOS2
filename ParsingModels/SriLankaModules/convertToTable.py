116149a92306012f5a3a6d182f8fe20eeaf846d8
JS <67136819+skyjune0824@users.noreply.github.com>
2024-02-02 10:12:55 -0500

    Update convertToTable.py

--- ParsingModels/SriLankaModules/convertToTable.py
+++ ParsingModels/SriLankaModules/convertToTable.py
@@ -2,7 +2,7 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../LLaMa')) # Gets the directory of LLaMaInterface module for import
-sys.path.append(os.path.join(os.path.dirname(__file__), '../Modules')) # Gets the directory of LLaMaInterface module for import
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../Modules')) # Gets the directory of LLaMaInterface module for import
 print(os.path.dirname(__file__))
 print(os.path.join(os.path.dirname(__file__), '../ParsingModels'))
 print(os.path.join(os.path.dirname(__file__), '../LLaMa'))
