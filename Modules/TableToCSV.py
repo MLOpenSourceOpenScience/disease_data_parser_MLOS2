fa98d2b1e819c9ffa3e765ab6854639710997708
JS <67136819+skyjune0824@users.noreply.github.com>
2024-02-05 14:18:55 +0900

    Update TableToCSV.py

--- Modules/TableToCSV.py
+++ Modules/TableToCSV.py
@@ -2,12 +2,16 @@ import os
 import csv
 from typing import *
 
-def printToCSV(table: List[List[str]], headings: List[str] = None, filePath: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../Out/output.csv')) -> None:
+def printToCSV(table: List[List[str]], headings: List[str] = None, fileName: str = '../Out/output.csv') -> None:
+
+    filePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), fileName)
+    # dynamic name available by user's taste
 
     fileDirectory = os.path.dirname(filePath)
     # if file is not available ( not existing on the first place )
 
     if not os.path.exists(fileDirectory):
+        # if there's no outputfile already, create one.
         os.makedirs(fileDirectory)
 
     with open(filePath, 'a', newline='') as file:
