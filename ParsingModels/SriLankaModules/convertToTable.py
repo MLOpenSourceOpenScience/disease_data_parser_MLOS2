9f79d4081d9fd75a72cf220702f51a2a9da10809
JS <67136819+skyjune0824@users.noreply.github.com>
2024-02-06 11:18:11 -0500

    import problem resolved

--- ParsingModels/SriLankaModules/convertToTable.py
+++ ParsingModels/SriLankaModules/convertToTable.py
@@ -2,7 +2,7 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../LLaMa')) # Gets the directory of LLaMaInterface module for import
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../Modules')) # Gets the directory of LLaMaInterface module for import
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../Modules')) # Gets the directory of Modules for import
 
 # from LLaMaInterface import separateCellHeaders
 from location_interface import get_location_info
--- dataParser.py
+++ dataParser.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 from Modules.PDFoperators import *
 from Modules.pdfExtractor import *
-from table_to_csv import *
+from Modules.table_to_csv import *
 
 
 if __name__ == "__main__":
