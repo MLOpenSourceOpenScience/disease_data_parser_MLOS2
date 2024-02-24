350d93ee7af34c8f7b903510a31fab20b6e400d3
Mahi <mahipasarkar@gmail.com>
2024-02-24 12:02:42 -0500

    opens up page properly and extracts text, now just iterate thru years

--- Data Scraping/Brazil/brazil_scraper.py
+++ Data Scraping/Brazil/brazil_scraper.py
@@ -2,29 +2,70 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
 import time
 
+def extract_from_disease_site(url: str):
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-data_website = 'https://datasus.saude.gov.br/acesso-a-informacao/doencas-e-agravos-de-notificacao-de-2007-em-diante-sinan/'
+    wait = WebDriverWait(driver, 10) # Set up wait for later
 
-dengue_2014_site = 'http://tabnet.datasus.gov.br/cgi/deftohtm.exe?sinannet/cnv/denguebbr.def'
+    #Window switching
+    original_window = driver.current_window_handle
 
-driver = webdriver.Chrome()
-driver.get(dengue_2014_site)
+    #Set settings
+    line = Select(driver.find_element(By.ID, "L"))
+    column = Select(driver.find_element(By.ID, "C"))
+    years = Select(driver.find_element(By.ID, "A"))
 
-#Set settings
-line = driver.find_element(By.ID, "L")
-column = driver.find_element(By.ID, "C")
-years = driver.find_element(By.ID, "A")
+    #Set settings properly
+    line.select_by_value("Município_de_residência")
+    column.select_by_value("Semana_epidem._1º_Sintomas(s)")
 
-# check format to be in Colunas separadas por ";"
-line = driver.find_element(By.ID, "F").click()
+    # check format to be in Colunas separadas por ";"
+    line = driver.find_element(By.XPATH, "//input[@value='prn']").click()
 
-#Show data
-driver.find_element(By.CLASS_NAME, "mostra").click()
+    years_done = []
 
-time.sleep(10)
+    #select each year
+    for i in range(len(years.options)-1):
+        years.select_by_index(i)   
+        current_year = years.first_selected_option.text
+        years_done.append(current_year)
 
-driver.close()
+        #Show data
+        driver.find_element(By.CLASS_NAME, "mostra").click()
+
+        #Wait for new tab to open
+        wait.until(EC.number_of_windows_to_be(2))
+
+        #Open new tab
+        for window_handle in driver.window_handles:
+                if window_handle != original_window:
+                    driver.switch_to.window(window_handle)
+                    break
+
+        #Extract text data  
+        data = driver.find_element(By.TAG_NAME, "pre").text
+        print(data)
+
+        driver.close()
+
+    print(years_done)
+    
+    time.sleep(10)
+
+    driver.close()
+
+
+
+if __name__ == '__main__':
+    data_website = 'https://datasus.saude.gov.br/acesso-a-informacao/doencas-e-agravos-de-notificacao-de-2007-em-diante-sinan/'
+
+    dengue_2014_site = 'http://tabnet.datasus.gov.br/cgi/deftohtm.exe?sinannet/cnv/denguebbr.def'
+
+    extract_from_disease_site(dengue_2014_site)
