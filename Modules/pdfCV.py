de0ca87ef94a06d5e449e8b8ade4a983ba175637
EggyRepublic <Eggyrepublic@gmail.com>
2024-02-28 13:39:50 -0500

    tried pdfminer doesn't work

--- Modules/pdfCV.py
+++ Modules/pdfCV.py
@@ -8,7 +8,7 @@ def convert_to_img(pdf_path):
     images = convert_from_path(pdf_path)
     for i, image in enumerate(images):
         image.save(pdf_path + f"page_{i}.jpg", 'JPEG')
-    return i
+    return i + 1
 
 def zoom(img, zoom_factor=0.5):
     return cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor)
@@ -20,9 +20,12 @@ def get_conv_function(filter):
 def conv(image):
     data = np.array(image)
 
-    filter = [-1,-0.5,0,0.5,1]
+    filter = [-10,-1,-0.5,0,0,0.5,1,10]
     result = np.apply_along_axis(get_conv_function(filter), 0, data)
-    threshold = 30
+    # switch to 2D conv
+    
+    print(f'result {result}')
+    threshold = 5
     result = np.where(result < threshold, 0, result)
 
     plt.imshow(result, interpolation='nearest')
@@ -40,6 +43,7 @@ def process(image_path):
     adaptive_thresh = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 3)
     #ret2,th2 = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     cv2.imshow('thresholded', adaptive_thresh)
+    conv(adaptive_thresh)
     cv2.waitKey(0)
 
     contours, _ = cv2.findContours(adaptive_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -50,7 +54,7 @@ def process(image_path):
     cv2.destroyAllWindows()
 
 def main():
-    input_file = '../Data/Dengue/sri lanka.pdf'
+    input_file = '../Data/Dengue/sri lanka page 3.pdf'
     print(input_file)
     images = convert_to_img(input_file)
     for i in range(images):
--- Modules/pdfExtractor.py
+++ Modules/pdfExtractor.py
@@ -1,6 +1,14 @@
 #Alan
 from PyPDF2 import PdfReader
 import os
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
 
 def PDFtoRTF(path, output_path=None):
     #Generate a text rendering of a PDF file in the form of a list of lines.
@@ -19,14 +27,34 @@ def PDFtoRTF(path, output_path=None):
             
     return full_text
 
+def PDFtoRTF2(path, output_path=None):
+    pdfminer_string = StringIO()
+    with open(path, "rb") as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr,
+                            pdfminer_string,
+                            laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+    pdfminer_lines = pdfminer_string.getvalue().splitlines()
+    pdfminer_lines = [ln for ln in pdfminer_lines if ln]
+
+    print(f'pdfMiner: {pdfminer_lines}')
+    if output_path is not None:
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(''.join(pdfminer_lines))
+
 def runner():
-    input_file = '../Data/Dengue/sri lanka.pdf'
+    input_file = '../Data/Dengue/sri lanka page 3.pdf'
     output_folder = '../Data/Dengue/RTF'
     print(input_file)
     output_file = input_file[input_file.rfind('/') + 1:input_file.rfind('.pdf')] + ".txt"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    print(PDFtoRTF(input_file, f"{output_folder}/{output_file}"))
+    PDFtoRTF(input_file, f"{output_folder}/{output_file}")
 
 if __name__ == '__main__':
     runner()
